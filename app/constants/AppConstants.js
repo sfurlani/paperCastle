41b4497cfaf0b38e29d19c5ab055f91a8349c876
Stephen Furlani <stephen.furlani@gmail.com>
2016-02-26 23:18:05 -0500

    Got Colors and new Background working

--- app/constants/AppConstants.js
+++ app/constants/AppConstants.js
@@ -1,4 +1,41 @@
 export const constants = {
-  RESIZE: 'APP_RESIZE',
-  ANIMATE: 'APP_ANIMATE'
+  RESIZE:  'APP_RESIZE',
+  ANIMATE: 'APP_ANIMATE',
+  COLORS:  {
+    // Solarized Colors: http://ethanschoonover.com/solarized#features
+    base03:    0x002b36,
+    base02:    0x073642,
+    base01:    0x586e75,
+    base00:    0x657b83,
+    base0:     0x839496,
+    base1:     0x93a1a1,
+    base2:     0xeee8d5,
+    base3:     0xfdf6e3,
+    yellow:    0xb58900,
+    orange:    0xcb4b16,
+    red:       0xdc322f,
+    magenta:   0xd33682,
+    violet:    0x6c71c4,
+    blue:      0x268bd2,
+    cyan:      0x2aa198,
+    green:     0x859900
+  }
+}
+
+export const THEME = {
+  // Solarized Colors: http://ethanschoonover.com/solarized#features
+  dark: {
+    background: constants.COLORS.base03,
+    highlight:  constants.COLORS.base02,
+    subtext:    constants.COLORS.base01,
+    body:       constants.COLORS.base0,
+    empphasis:  constants.COLORS.base1
+  },
+  light: {
+    background: constants.COLORS.base3,
+    highlight:  constants.COLORS.base2,
+    subtext:    constants.COLORS.base1,
+    body:       constants.COLORS.base00,
+    empphasis:  constants.COLORS.base01
+  }
 }
--- app/displayobjects/Background/Background.js
+++ app/displayobjects/Background/Background.js
@@ -1,5 +1,7 @@
 import PIXI from 'pixi.js';
-import TEXTURE from './diagnostic.png';
+// import TEXTURE from './diagnostic.png';
+import { THEME } from '../../constants/AppConstants';
+import RendererStore from '../../stores/RendererStore';
 
 /**
  * Loads the adds the diagnostic image
@@ -12,7 +14,10 @@ export default class Background extends PIXI.Container {
   constructor() {
     super();
 
-    let bg = PIXI.Sprite.fromImage(TEXTURE);
+    // let bg = PIXI.Sprite.fromImage(TEXTURE);
+    let bg = new PIXI.Graphics();
+    bg.beginFill(THEME.light.background,1);
+    bg.drawRect(0,0,RendererStore.get('stageWidth'),RendererStore.get('stageHeight'));
 
     this.addChild(bg);
   }
